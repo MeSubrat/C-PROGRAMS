6096ad20de9994e216694f775974d4fd086b5df8
MeSubrat <subratksahoo86@gmail.com>
2023-10-12 20:53:04 +0530

    Second commit

--- BinarySearch.c
+++ BinarySearch.c
@@ -2,11 +2,21 @@
 int binarySearch(int arr[],int high,int low,int mid,int value);
 int main()
 {
-    int arr[]={10,20,30,40,50,60};
-    printf("Enter the element to be found : ");
+    // int arr[]={10,20,30,40,50,60};
+    // printf("Enter the element to be found : ");
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter elements to the array :");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
     int value;
+    printf("Enter the element to be found : ");
     scanf("%d",&value);
-    int high=4;
+    int high=n-1;
     int low=0;
     int mid=(high+low)/2;
     int location=binarySearch(arr,high,low,mid,value);
